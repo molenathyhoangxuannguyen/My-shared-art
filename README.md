c73f065c1f9c55f53954ef73e9bf6fe97a1930d7
Thy H. Nguyen <46432072+nguyent2@users.noreply.github.com>
2020-01-16 21:44:54 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# T02: Our Shared Art
+
+## by Thy Nguyen*
+##    Betrina Iransi*
+
+## References:
+- *Add referenece(s) to all art that inspired this submission or state that this is original work.* 
---
+++ shared-art-main.cpp
@@ -0,0 +1,64 @@
+/* T03: Our Shared Art
+Teamname: BT
+Partner1: Thy Nguyen
+Partner 2: Betrina Iransi */  
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	char holdme;
+
+
+		cout << "\\``." << endl;
+	cout << "\_`.``-." << endl;
+	cout << "( `.`.` `._" << endl;
+	cout << "`._`-.    `._" << endl;
+	cout << "\`--.   ,' `." << endl;
+	cout << " `--._  `.  .`." << endl;
+	cout << "    `--.--- `. ` `." << endl;
+	cout << " `.--  `;  .`._" << endl;
+	cout << "  :-   :   ;. `.__,.,__ __" << endl;
+	cout << "   `\  :       ,-(     ';o`>." << endl;
+	cout << "`-.`:   ,'   `._ .:  (,-`," << endl;
+	cout << "   \    ;      ;.  ,:" << endl;
+	cout << "  ,'`-._>-:        ;,'  `---.,---." << endl;
+	cout << " `>''  '-`       ,'   "":::::'.. `-." << endl;
+	cout << "  `;''_,  (\`\ _ `:::::::::::''     `---." << endl;
+	cout << "    `-(_,' -'),)\`.       _      .::::''  `----._,-'')" << endl;
+	cout << "           \_,': `.-' `-----' `--;-.   `.   ``.`--.____/" << endl;
+	cout << "             `-^--'                \(-.  `.``-.`-=:-.__)" << endl;
+	cout << "                                    `  `.`.`._`.-._`--.)" << endl;
+	cout << "                                                 `-^---^--.`--" << endl;
+		
+
+
+/*
+	cout << "           _____________________________________ " << endl;
+	cout << "         //_________________(o)_________________\\ " << endl;
+	cout << endl;
+	cout << "         || > Hello Students!                   || " << endl;
+	cout << endl;
+	cout << "         || > Welcome again to CSC 236!         || " << endl;
+	cout << "            > We're happy you are here," << endl;
+	cout << "         || > and want to help!                 || " << endl;
+	cout << "                       - Dr. Jan and TAs" << endl;
+	cout << "         ||                                     || " << endl;
+	cout << endl;
+	cout << "         ||_____________________________________|| " << endl;
+	cout << "        /_______________________________________ \\ " << endl;
+	cout << "       /_| _| _| _| _| _| _| _| _| _| _| _| _| _| \\ " << endl;
+	cout << "      /____| _| _| _| _| _| _| _| _| _| _| _| ___| \\ " << endl;		
+	cout << "     /___| _| _| _| _| _| _| _| _| _| _| _| _| ___| \\ " << endl;
+	cout << "    /__| _| _| _| _| _| _| _| _| _| _| _| _| _| ___  \\ " << endl;
+	cout << "   /_| _| _| _| _|______________________|  _| _|_|_|  \\ " << endl;
+	cout << "  /________________/                  \\________________\\ " << endl;
+	cout << " (________________/____________________\\_______________) " << endl;
+
+
+
+*/
+
+	cin >> holdme; //Keeps window open in some cases
+	return 0;
+}
